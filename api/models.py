0e6262db076375e23149db6f526cd898850358e2
shreyrohatgi <rohatgishrey@gmail.com>
2019-09-03 00:27:52 +0530

    Added location model and views

--- api/models.py
+++ api/models.py
@@ -5,10 +5,11 @@ from rest_framework.exceptions import ValidationError
 from datetime import date
 
 userTypes = ( ('admin','admin'),('dda','dda'), ('ado', 'ado') )
+status = ( ('pending','pending'),('ongoing','ongoing'),('completed','completed') )
 
 class User(models.Model):
     name = models.CharField(max_length = 100, blank = False, null = False, unique = False)
-    typeOfUser = models.CharField(max_length=10,choices=userTypes,default='intern')
+    typeOfUser = models.CharField(max_length=10,choices=userTypes,default='admin')
     auth_user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
     
     def __str__(self):
@@ -18,3 +19,16 @@ class User(models.Model):
             return self.auth_user.username
     class Meta:
         ordering = ["name"]
+
+class Location(models.Model):
+    state = models.CharField(max_length = 50, blank = False, null = False, unique = False)
+    district = models.CharField(max_length = 50, blank = False, null = False, unique = False)
+    block_name = models.CharField(max_length = 50, blank = False, null = False, unique = False)
+    village_name = models.CharField(max_length = 100, blank = False, null = False, unique = False)
+    longitude = models.CharField(max_length = 100, blank = False, null = False, unique = False)
+    latitude = models.CharField(max_length = 100, blank = False, null = False, unique = False)
+    acq_Date = models.DateTimeField(default = timezone.now)
+    acq_Time = models.DateTimeField(default = timezone.now)
+    ddo = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True, related_name = 'ddo')        
+    ado = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True, default = 'null', related_name = 'ado')
+    typeofChoice = models.CharField(max_length = 10, choices = status, default = 'pending') 
--- api/views.py
+++ api/views.py
@@ -66,3 +66,58 @@ class GetUser(APIView):
         user = User.objects.get(auth_user = request.user.pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+class LocationList(APIView):
+    def get(self, request, format = None):
+        Location = Location.objects.all()
+        serializer = LocationSerializer(Location, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format = None):
+        #username = request.data.get('username')
+        #password = request.data.get('password') 
+        #request.data.pop('username')
+        #request.data.pop('password')
+
+        directory = MEDIA_ROOT + '/locationCSVs/'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        locationNums = []
+        if 'location_csv' in data:
+            if not data['location_csv'].name.endswith('.csv'):
+                return Response({'location_csv': ['Please upload a valid document ending with .csv']},
+                    status = HTTP_400_BAD_REQUEST)
+            fs = FileSystemStorage()
+            fs.save(directory + data['location_csv'].name.data['location_csv'])
+            csvFile = open(directory + data['location_csv'.name, 'r'])
+            for location_num in csvFile.readlines():
+                locationNums.append(location_num.strip())
+            locationNums.pop(0)
+
+        for lines in locationNums:
+            try:
+                array = lines.split(',')
+                first_item = array[0]
+                num_columns = len(array)
+                csvFile.seek(0)
+            except ValueError:
+               return Response({'location_csv': [
+                    'CSV format is not correct.']},
+                    status = HTTP_400_BAD_REQUEST)
+
+        print(locationNums)       
+    
+            
+        try:
+            django_user_obj = DjangoUser.objects.create(username=username)
+        except IntegrityError as e:
+            raise ValidationError(str(e))
+        django_user_obj.set_password(password)
+        django_user_obj.save()
+        request.data['auth_user'] = django_user_obj
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
