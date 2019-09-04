bce12e61b9c50ee819b3ef3efd79a3dd1e76998c
Mohit Nagpal <nagpalm7@gmail.com>
2019-09-04 19:47:17 +0530

    ussign location to specific ado

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('api/get-user/', views.GetUser.as_view(), name='get-user'),
     path('api/user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
     path('api/upload/locations/', views.LocationList.as_view(), name='user-detail'),
+    path('api/location/<int:pk>/', views.LocationDetail.as_view(), name='location-detail'),
     path('api/locations/<str:status>', views.LocationViewSet.as_view({'get': 'list'}), name='admin-location-list'),
     path('api/locations/ado/<str:status>', views.LocationViewSetAdo.as_view({'get': 'list'}), name='ado-location-list'),
     path('api/locations/dda/<str:status>', views.LocationViewSetDda.as_view({'get': 'list'}), name='dda-location-list'),
--- api/views.py
+++ api/views.py
@@ -63,6 +63,33 @@ class UserDetail(APIView):
         user.delete()
         return Response(status=HTTP_204_NO_CONTENT)
 
+# Details of particular location and edit location in order to set ado
+class LocationDetail(APIView):
+    permission_classes = []
+    def get_object(self, pk):
+        try:
+            return Location.objects.get(pk=pk)
+        except Location.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format = None):
+        location = self.get_object(pk)
+        serializer = LocationSerializer(location)
+        return Response(serializer.data)
+
+    def patch(self, request, pk, format = None):
+        location = self.get_object(pk)
+        serializer = LocationSerializer(location, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format = None):
+        location = self.get_object(pk)
+        location.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
 class GetUser(APIView):
     permission_classes = [IsAuthenticated,]
     def get(self, request, format = None):
