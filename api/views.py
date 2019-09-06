9c54fc9bcdace4cb215c283955746c3809485056
shreyrohatgi <rohatgishrey@gmail.com>
2019-09-06 22:28:02 +0530

    Adding views

--- api/views.py
+++ api/views.py
@@ -63,6 +63,56 @@ class UserDetail(APIView):
         user.delete()
         return Response(status=HTTP_204_NO_CONTENT)
 
+class AdminList(APIView):
+    pagination_class = StandardResultsSetPagination
+    def get(self, request, format = None):
+        admin = Admin.objects.all()
+        serializer = AdminSerializer(admin, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format = None):
+        username = request.data.get('username')
+        password = request.data.get('password') 
+        request.data.pop('username')
+        request.data.pop('password')
+        try:
+            django_user_obj = DjangoUser.objects.create(username=username)
+        except IntegrityError as e:
+            raise ValidationError(str(e))
+        django_user_obj.set_password(password)
+        django_user_obj.save()
+        request.data['auth_user'] = django_user_obj
+        serializer = AdminSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AdminDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Admin.objects.get(pk=pk)
+        except Admin.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format = None):
+        admin = self.get_object(pk)
+        serializer = AdminSerializer(admin)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format = None):
+        admin = self.get_object(pk)
+        serializer = AdminSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format = None):
+        admin = self.get_object(pk)
+        admin.delete()
+        return Response(status=HTTP_204_NO_CONTENT)            
+
 # Details of particular location and edit location in order to set ado
 class LocationDetail(APIView):
     permission_classes = []
